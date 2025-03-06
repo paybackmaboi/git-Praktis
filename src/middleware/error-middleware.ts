4e9d5f0cf765b00295529df57c546f0bfa21198f
unknown <RomyJr2003>
2025-03-06 15:05:21 +0800

    changes of middleware

--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-    const statusCode = typeof err === 'string' && err.toLowerCase().endsWith('not found') ? 404 : 500;
+    const statusCode = typeof err === 'string' && err.toLowerCase().endsWith('student not found') ? 404 : 500;
     res.status(statusCode).json({ message: err.message || err });
 }
 
--- src/middleware/validate-middleware.ts
+++ src/middleware/validate-middleware.ts
@@ -4,7 +4,7 @@ import Joi from 'joi';
 export function validateRequest(req: Request, next: NextFunction, schema: Joi.Schema) {
     const { error, value } = schema.validate(req.body, { abortEarly: false, allowUnknown: true, stripUnknown: true });
     if (error) {
-        next(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
+        next(`student Validation error: ${error.details.map(x => x.message).join(', ')}`);
     } else {
         req.body = value;
         next();
